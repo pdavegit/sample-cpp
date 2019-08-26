d24917241445d1653df59eadd6478e619323a85b
Prashant Dave <prashant.dave.email@gmail.com>
2019-08-26 19:16:44 -0400

    Updated Jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,20 +1,11 @@
 node('') {
     checkout scm
     stage('build-ubuntu') {
-       sh 'docker pull prashantdave/ubuntu-cpp-env:18.04'    
+       //sh 'docker pull prashantdave/ubuntu-cpp-env:18.04'    
         docker.withRegistry('https://index.docker.io/v1') {
             docker.image('prashantdave/ubuntu-cpp-env:18.04').inside {
-                sh 'cd src && make -f Makefile.ubuntu all'
-		sh 'cd src && make -f Makefile.ubuntu test'
+                sh 'cd src && docker build -t prashantdave/ubuntu-cpp-fcgi-app:latest -f Dockerfile-ubuntu-cpp-fcgi-app.build .'
             }
         }
     }
-/*    stage('build-alpine') {
-       sh 'docker pull prashantdave/alpine-cpp-env:latest'	
-        docker.withRegistry('https://index.docker.io/v1') {
-            docker.image('prashantdave/alpine-cpp-env:latest').inside {
-                sh 'make -f Makefile.alpine all'
-            }
-        }
-    }  */   
 }
--- src/create-build-env.sh
+++ src/create-build-env.sh
@@ -3,7 +3,6 @@ exit
 docker build -t prashantdave/ubuntu-cpp-env:18.04 -f Dockerfile-ubuntu-cpp-env.build .
 docker build -t prashantdave/ubuntu-cpp-fcgi-env:18.04 -f Dockerfile-ubuntu-cpp-fcgi-env.build .
 docker build -t prashantdave/ubuntu-cpp-fcgi-app:latest -f Dockerfile-ubuntu-cpp-fcgi-app.build .
-
 docker push prashantdave/ubuntu-cpp-fcgi-env:18.04
 
 docker run -p 8001:8001 -t prashantdave/ubuntu-cpp-fcgi
@@ -15,3 +14,19 @@ docker build -f Dockerfile.alpine.build .
 docker build -f Dockerfile.ubuntu.build .
 
 docker run -p 8080:8080 -it jenkins/jenkins:lts
+
+docker build -t prashantdave/jenkins-docker:latest -f Dockerfile-jenkins .
+#jenkins/jenkins:lts 
+docker run -p 8080:8080 \
+      --mount type=bind,source=$JENKINS_HOME,target=/var/jenkins_home \
+      --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock \
+      -it prashantdave/jenkins-docker:latest
+
+docker exec -u 0 -it <container> /bin/bash
+# apt-get update && apt-get install docker
+# exit
+docker ps -a
+docker commit 
+
+
+
